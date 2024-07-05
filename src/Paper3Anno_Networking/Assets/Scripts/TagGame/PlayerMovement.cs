10b38c175f1362b9f71cd6df1b97af797d8ef192
Gioele Giusti <gioele.pg@gmail.com>
2024-07-05 10:41:17 +0200

    Update movemenent && add glasses to player

--- src/Paper3Anno_Networking/Assets/Scripts/TagGame/PlayerMovement.cs
+++ src/Paper3Anno_Networking/Assets/Scripts/TagGame/PlayerMovement.cs
@@ -1,20 +1,36 @@
 using Unity.Netcode;
 using UnityEngine;
+
 public class PlayerNetwork : NetworkBehaviour
 {
-    public float speed;
+    public float movementSpeed;
+    public float rotationSpeed;
 
     private void Update()
     {
         if (!IsOwner) {
             return;
         }
+
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
 
-            var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            transform.Translate(movement * (speed * Time.deltaTime));
+            var movementDirection = new Vector3(moveHorizontal, 0, moveVertical);
+            movementDirection.Normalize();
+
+            transform.Translate(movementDirection * (movementSpeed * Time.deltaTime), Space.World);
+
+            if (movementDirection != Vector3.zero) {
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+                transform.rotation =
+                    Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
