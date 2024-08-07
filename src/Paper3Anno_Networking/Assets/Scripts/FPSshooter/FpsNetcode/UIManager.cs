a0d70265bf7fb4b98a38e2821e1fb9eded54e70f
Gioele Giusti <gioele.pg@gmail.com>
2024-08-07 19:38:24 +0200

    Add Logger text script to show debug message in display

--- src/Paper3Anno_Networking/Assets/Scripts/FPSshooter/FpsNetcode/UIManager.cs
+++ src/Paper3Anno_Networking/Assets/Scripts/FPSshooter/FpsNetcode/UIManager.cs
@@ -26,26 +26,32 @@ namespace FPShooter
             startServerButton.onClick.AddListener(() => {
                 if (NetworkManager.Singleton.StartServer()) {
                     Logger.Instance.LogInfo("Server started ...");
+                    LoggerText.Instance.LogText("Server started ...");
                 }
                 else {
                     Logger.Instance.LogInfo("Server could not started ...");
+                    LoggerText.Instance.LogText2("Server could not started ...");
                 }
             });
             startHostButton.onClick.AddListener(() => {
                 Debug.Log("host button clicked");
                 if (NetworkManager.Singleton.StartHost()) {
                     Logger.Instance.LogInfo("Host started ...");
+                    LoggerText.Instance.LogText("Host started ...");
                 }
                 else {
                     Logger.Instance.LogInfo("Host could not be started ...");
+                    LoggerText.Instance.LogText2("Host could not be started ...");
                 }
             });
             startClientButton.onClick.AddListener(() => {
                 if (NetworkManager.Singleton.StartClient()) {
                     Logger.Instance.LogInfo("Client started ...");
+                    LoggerText.Instance.LogText("Client started ...");
                 }
                 else {
                     Logger.Instance.LogInfo("Client could not started ...");
+                    LoggerText.Instance.LogText2("Client could not started ...");
                 }
             });
         }
